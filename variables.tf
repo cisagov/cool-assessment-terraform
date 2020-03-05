e9e586b29a96394143e37d865e643871aaaa4c75
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-05 16:42:13 -0500

    Add new variables

--- variables.tf
+++ variables.tf
@@ -4,12 +4,25 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)"
+variable "assessment_account_name" {
+  description = "The name of the AWS account for this assessment."
 }
 
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
+variable "private_domain" {
+  description = "The local domain to use for this assessment."
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "The list of private subnet CIDR blocks for this assessment."
+  type        = list(string)
+}
+
+variable "public_subnet_cidr_block" {
+  description = "The public subnet CIDR block for this assessment."
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block to use this assessment's VPC."
 }
 
 # ------------------------------------------------------------------------------
@@ -17,6 +30,31 @@ variable "aws_availability_zone" {
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
+
+variable "aws_availability_zone" {
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
+  default     = "a"
+}
+
+variable "aws_region" {
+  description = "The AWS region where the non-global resources for this assessment are to be provisioned (e.g. \"us-east-1\")."
+  default     = "us-east-1"
+}
+
+variable "provisionaccount_role_name" {
+  description = "The name of the IAM role that allows sufficient permissions to provision all AWS resources in the assessment account."
+  default     = "ProvisionAccount"
+}
+
+variable "provisionassessment_policy_description" {
+  description = "The description to associate with the IAM policy that allows provisioning of the resources required in the assessment account."
+  default     = "Allows provisioning of the resources required in the assessment account."
+}
+
+variable "provisionassessment_policy_name" {
+  description = "The name to assign the IAM policy that allows provisioning of the resources required in the assessment account."
+  default     = "ProvisionAssessment"
+}
 
 variable "tags" {
   type        = map(string)
