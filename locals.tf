059cc10a9f1ecd35f61dcf3536823db4269fbd8e
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-05 17:08:33 -0500

    Add locals

---
+++ locals.tf
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Retrieve the effective Account ID, User ID, and ARN in which Terraform is
+# authorized.  This is used to calculate the session names for assumed roles.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+
+# ------------------------------------------------------------------------------
+# Retrieve the information for all accouts in the organization.  This is used
+# to lookup the Users account ID for use in the assume role policy.
+# ------------------------------------------------------------------------------
+data "aws_organizations_organization" "cool" {
+  provider = aws.organizationsreadonly
+}
+
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this configuration.
+# ------------------------------------------------------------------------------
+locals {
+  # Extract the user name of the current caller for use
+  # as assume role session names.
+  caller_user_name = split("/", data.aws_caller_identity.current.arn)[1]
+
+  # Find the new Users account by name and email.
+  users_account_id = [
+    for x in data.aws_organizations_organization.cool.accounts :
+    x.id if x.name == "Users" && length(regexall("2020", x.email)) > 0
+  ][0]
+}
