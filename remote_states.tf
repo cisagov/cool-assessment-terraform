4ff8735f9ecdd0ae67e80a0bacb4e84263ee2230
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-06 17:08:55 -0500

    Add dns_certboto remote state

--- remote_states.tf
+++ remote_states.tf
@@ -3,6 +3,20 @@
 # root-level outputs of one or more Terraform configurations as input data
 # for this configuration.
 # ------------------------------------------------------------------------------
+data "terraform_remote_state" "dns_certboto" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-dns-certboto/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
 
 data "terraform_remote_state" "dynamic_assessment" {
   backend = "s3"
