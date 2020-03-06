c45133601750f598702bfd424d283277350437d4
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-06 17:10:27 -0500

    Add assessment_account_id and guacamole_fqdn

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
 # ------------------------------------------------------------------------------
+# Retrieve the caller identity for the current assessment in order to
+# get the associated Account ID.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "assessment" {
+  provider = aws.provisionassessment
+}
+
+# ------------------------------------------------------------------------------
 # Retrieve the effective Account ID, User ID, and ARN in which Terraform is
 # authorized.  This is used to calculate the session names for assumed roles.
 # ------------------------------------------------------------------------------
@@ -16,11 +24,16 @@ data "aws_organizations_organization" "cool" {
 # Evaluate expressions for use throughout this configuration.
 # ------------------------------------------------------------------------------
 locals {
+  # The account ID for this assessment
+  assessment_account_id = data.aws_caller_identity.assessment.account_id
+
   # Extract the user name of the current caller for use
   # as assume role session names.
   caller_user_name = split("/", data.aws_caller_identity.current.arn)[1]
 
   cool_shared_services_cidr_block = data.terraform_remote_state.sharedservices_networking.outputs.vpc.cidr_block
+
+  guacamole_fqdn = format("guac.%s.%s", var.assessment_account_name, var.cool_domain)
 
   # Helpful lists for defining ACL and security group rules
   ingress_and_egress = [
