7e65ad9acda116d880558a2f5da9a507552998b0
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-09 16:50:38 -0400

    Add additional permissions that are required to start up an EC2 instance from an encrypted AMI

--- provisionassessment_policy.tf
+++ provisionassessment_policy.tf
@@ -42,6 +42,7 @@ data "aws_iam_policy_document" "provisionassessment_policy_doc" {
       "ec2:DisassociateVpcCidrBlock",
       "ec2:GetTransitGatewayRouteTableAssociations",
       "ec2:GetTransitGatewayRouteTablePropagations",
+      "ec2:ModifyInstanceAttribute",
       "ec2:ModifyTransitGatewayVpcAttachment",
       "ec2:ModifyVpcAttribute",
       "ec2:ReleaseAddress",
@@ -49,14 +50,30 @@ data "aws_iam_policy_document" "provisionassessment_policy_doc" {
       "ec2:ReplaceNetworkAclEntry",
       "ec2:ReplaceRoute",
       "ec2:ReplaceRouteTableAssociation",
+      "ec2:ReportInstanceStatus",
       "ec2:RevokeSecurityGroupEgress",
       "ec2:RevokeSecurityGroupIngress",
+      "ec2:RunInstances",
+      "ec2:StartInstances",
+      "ec2:StopInstances",
+      "ec2:TerminateInstances",
       "ec2:UpdateSecurityGroupRuleDescriptionsEgress",
       "ec2:UpdateSecurityGroupRuleDescriptionsIngress",
     ]
 
     resources = [
       "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom",
+    ]
+
+    resources = [
+      data.terraform_remote_state.images.outputs.ami_kms_key.arn
     ]
   }
 }
