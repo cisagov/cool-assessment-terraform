febed5a60bd2fbd577aa3ee131396e62ca8b0e00
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-09 10:49:52 -0400

    Add provider used to create roles that can read parameter data from an SSM Parameter Store

--- providers.tf
+++ providers.tf
@@ -27,6 +27,17 @@ provider "aws" {
   }
 }
 
+# The provider used to create roles that can read parameter data
+# from an SSM Parameter Store
+provider "aws" {
+  alias  = "provisionparameterstorereadrole"
+  region = var.aws_region
+  assume_role {
+    role_arn     = data.terraform_remote_state.images_parameterstore.outputs.provisionparameterstorereadroles_role.arn
+    session_name = local.caller_user_name
+  }
+}
+
 # The provider used to lookup account IDs.  See locals.
 provider "aws" {
   alias  = "organizationsreadonly"
