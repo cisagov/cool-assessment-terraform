0677acd8b8ec36508c2bb6e58c60efa89956025a
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-10 15:53:50 -0400

    Attach CloudwatchAgentServerPolicy to the Guacamole instance profile role.

--- guacamole_iam.tf
+++ guacamole_iam.tf
@@ -37,6 +37,14 @@ resource "aws_iam_role_policy" "guacamole_assume_delegated_role_policy" {
   policy = data.aws_iam_policy_document.guacamole_assume_delegated_role_policy_doc.json
 }
 
+# Attach the CloudWatch Agent policy to this role as well
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment" {
+  provider = aws.provisionassessment
+
+  role       = aws_iam_role.guacamole_instance_role.id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 ################################
 # Define the role policies below
 ################################
