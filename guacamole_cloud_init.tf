0385e24b24d1f9701fe71316be7d8b744098949a
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-13 15:46:57 -0400

    Make cloud-init file parts specific to Kali instance ("kali0").

--- guacamole_cloud_init.tf
+++ guacamole_cloud_init.tf
@@ -21,31 +21,36 @@ data "template_cloudinit_config" "guacamole_cloud_init_tasks" {
   }
 
   # TODO: Make this more generalized and able to support a variety of connections
+  # Set up Guacamole connection to Kali instance
   part {
-    filename     = "write-guac-connection-sql-template.yml"
+    filename     = "write-kali-guac-connection-sql-template.yml"
     content_type = "text/cloud-config"
     content = templatefile(
       "${path.module}/cloud-init/write-guac-connection-sql-template.tpl.yml", {
-        guac_connection_name  = var.guac_connection_name
+        instance_hostname     = "kali0"
         private_domain        = var.private_domain
-        sql_template_fullpath = "/root/guacamole_connection_template.sql"
+        sql_template_fullpath = "/root/kali_guacamole_connection_template.sql"
     })
   }
 
   # TODO: Make this more generalized and able to support a variety of connections
+  # Set up Guacamole connection to Kali instance
+  # NOTE: Postgres processes initialization files alphabetically, so it's
+  # important to name guac_connection_setup_filename so it runs after the
+  # file that defines the Guacamole tables and users ("00_initdb.sql").
   part {
     filename     = "render-guac-connection-sql-template.py"
     content_type = "text/x-shellscript"
     content = templatefile(
       "${path.module}/cloud-init/render-guac-connection-sql-template.py", {
         aws_region                       = var.aws_region
-        guac_connection_setup_filename   = var.guac_connection_setup_filename
+        guac_connection_setup_filename   = "01_setup_kali_guac_connection.sql"
         guac_connection_setup_path       = var.guac_connection_setup_path
         ssm_vnc_read_role_arn            = aws_iam_role.vnc_parameterstorereadonly_role.arn
         ssm_key_vnc_password             = var.ssm_key_vnc_password
         ssm_key_vnc_user                 = var.ssm_key_vnc_username
         ssm_key_vnc_user_private_ssh_key = var.ssm_key_vnc_user_private_ssh_key
-        sql_template_fullpath            = "/root/guacamole_connection_template.sql"
+        sql_template_fullpath            = "/root/kali_guacamole_connection_template.sql"
     })
   }
 }
