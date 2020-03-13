c10637ebd39deef5f5a0e20d93b6a734551695a6
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-13 13:47:47 -0400

    Allow port 443 in instead of port 8443

--- desktop_gateway_sg_rules.tf
+++ desktop_gateway_sg_rules.tf
@@ -39,9 +39,9 @@ resource "aws_security_group_rule" "desktop_gw_egress_to_anywhere_via_https" {
 }
 
 # Allow ingress from COOL Shared Services VPN server CIDR block
-# via port 8443 (nginx/guacamole web)
+# via port 443 (nginx/guacamole web)
 # For: Assessment team access to Guacamole web client
-resource "aws_security_group_rule" "desktop_gw_ingress_from_trusted_via_port_8443" {
+resource "aws_security_group_rule" "desktop_gw_ingress_from_trusted_via_port_443" {
   provider = aws.provisionassessment
 
   security_group_id = aws_security_group.desktop_gateway.id
@@ -49,8 +49,8 @@ resource "aws_security_group_rule" "desktop_gw_ingress_from_trusted_via_port_844
   protocol          = "tcp"
   cidr_blocks       = [local.vpn_server_cidr_block]
   # ipv6_cidr_blocks  = TBD
-  from_port = 8443
-  to_port   = 8443
+  from_port = 443
+  to_port   = 443
 }
 
 # Allow egress via VNC to the Operations subnet
@@ -67,10 +67,9 @@ resource "aws_security_group_rule" "desktop_gw_egress_to_ops_via_vnc" {
   to_port           = 5901
 }
 
-# Allow ingress from anywhere via ephemeral ports below 8443 (1024-8442)
-# We do not want to allow everyone to hit Guacamole on port 8443
+# Allow ingress from anywhere via ephemeral ports
 # For: Guacamole fetches its SSL certificate via boto3 (which uses HTTPS)
-resource "aws_security_group_rule" "desktop_gw_ingress_from_anywhere_via_ports_1024_thru_8442" {
+resource "aws_security_group_rule" "desktop_gw_ingress_from_anywhere_via_ephemeral_ports" {
   provider = aws.provisionassessment
 
   security_group_id = aws_security_group.desktop_gateway.id
@@ -78,19 +77,5 @@ resource "aws_security_group_rule" "desktop_gw_ingress_from_anywhere_via_ports_1
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 1024
-  to_port           = 8442
-}
-
-# Allow ingress from anywhere via ephemeral ports above 8443 (8444-65535)
-# We do not want to allow everyone to hit Guacamole on port 8443
-# For: Guacamole fetches its SSL certificate via boto3 (which uses HTTPS)
-resource "aws_security_group_rule" "desktop_gw_ingress_from_anywhere_via_ports_8444_thru_65535" {
-  provider = aws.provisionassessment
-
-  security_group_id = aws_security_group.desktop_gateway.id
-  type              = "ingress"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 8444
   to_port           = 65535
 }
