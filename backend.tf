cceb3dda7a4f80d6d43bbca905b751199890df3c
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-16 17:25:45 -0400

    Use a single key for the Terraform backend, rather than using one key per dynamic account.

--- backend.tf
+++ backend.tf
@@ -5,9 +5,6 @@ terraform {
     dynamodb_table = "terraform-state-lock"
     profile        = "cool-terraform-backend"
     region         = "us-east-1"
-    # Locally change DYNAMIC_ACCOUNT_NAME below
-    # to the name of the new environment
-    # TODO: https://github.com/cisagov/cool-accounts/issues/34
-    key = "cool-assessment-terraform/DYNAMIC_ACCOUNT_NAME.tfstate"
+    key            = "cool-assessment-terraform/terraform.tfstate"
   }
 }
