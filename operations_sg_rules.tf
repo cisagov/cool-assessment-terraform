8ed22fdd355a7eaa5aee471f35790cac5604f72a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-01 22:35:55 -0400

    Remove ephemeral ingress declarations from security groups
    Security groups are effectively stateful firewalls, and this do not
    require ephemeral port ingress to be declared.  See, for example, the
    sixth bullet listed here:
    https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html#VPCSecurityGroups

--- operations_sg_rules.tf
+++ operations_sg_rules.tf
@@ -39,51 +39,6 @@ resource "aws_security_group_rule" "operations_ingress_from_anywhere_via_allowed
   to_port           = each.value
 }
 
-# Allow ingress from anywhere via ephemeral TCP/UDP ports below 3389 (1024-3388)
-# For: Assessment team operational use, but don't want to allow
-#      public access to RDP on port 3389
-resource "aws_security_group_rule" "operations_ingress_from_anywhere_via_ports_1024_thru_3388" {
-  provider = aws.provisionassessment
-  for_each = toset(local.tcp_and_udp)
-
-  security_group_id = aws_security_group.operations.id
-  type              = "ingress"
-  protocol          = each.value
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 1024
-  to_port           = 3388
-}
-
-# Allow ingress from anywhere via ephemeral TCP/UDP ports 3390-5900
-# For: Assessment team operational use, but don't want to allow
-#      public access to RDP on port 3389 or VNC on port 5901
-resource "aws_security_group_rule" "operations_ingress_from_anywhere_via_ports_3390_thru_5900" {
-  provider = aws.provisionassessment
-  for_each = toset(local.tcp_and_udp)
-
-  security_group_id = aws_security_group.operations.id
-  type              = "ingress"
-  protocol          = each.value
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 3390
-  to_port           = 5900
-}
-
-# Allow ingress from anywhere via ephemeral TCP/UDP ports above 5901 (5902-65535)
-# For: Assessment team operational use, but don't want to allow
-#      public access to VNC on port 5901
-resource "aws_security_group_rule" "operations_ingress_from_anywhere_via_ports_5902_thru_65535" {
-  provider = aws.provisionassessment
-  for_each = toset(local.tcp_and_udp)
-
-  security_group_id = aws_security_group.operations.id
-  type              = "ingress"
-  protocol          = each.value
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 5902
-  to_port           = 65535
-}
-
 # Allow ingress from anywhere via ICMP
 # For: Assessment team operational use (e.g. ping responses)
 resource "aws_security_group_rule" "operations_ingress_from_anywhere_via_icmp" {
