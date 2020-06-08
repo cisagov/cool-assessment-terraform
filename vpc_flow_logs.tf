50a810606ce783bb5714d03c061748089e5d4d1b
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-07 22:01:09 -0400

    Pass the correct provider to the vpc-flow-logs module

--- vpc_flow_logs.tf
+++ vpc_flow_logs.tf
@@ -3,6 +3,9 @@
 #-------------------------------------------------------------------------------
 module "vpc_flow_logs" {
   source = "trussworks/vpc-flow-logs/aws"
+  providers = {
+    aws = aws.provisionassessment
+  }
 
   vpc_name       = local.assessment_account_name_base
   vpc_id         = aws_vpc.assessment.id
