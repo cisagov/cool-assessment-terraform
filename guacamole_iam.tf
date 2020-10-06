c25b9d4cde1d208002f409cc574c494facf7c0bf
Dave Redmin <david.redmin@beta.dhs.gov>
2020-10-06 09:24:51 -0400

    Point to testing branch of cert-read-role-tf-module

--- guacamole_iam.tf
+++ guacamole_iam.tf
@@ -1,6 +1,6 @@
 # Create a role that allows the instance to read its certs from S3.
 module "guacamole_certreadrole" {
-  source = "github.com/cisagov/cert-read-role-tf-module"
+  source = "github.com/cisagov/cert-read-role-tf-module?ref=test_tf_aws_provider_v3"
 
   providers = {
     aws = aws.provisioncertreadrole
