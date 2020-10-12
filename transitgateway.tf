016ce24b5e7985e3c5078047324b0926a08b9477
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-10-12 14:26:53 -0400

    Disassociate the TGW VPC attachment and the TGW's default route table
    We do this using the AWS CLI via a null_resource with a local-exec
    provisioner.  It would be nice if we didn't have to use this escape
    hatch, but Terraform does not support this sort of cross-account foo
    right now.
    
    Note that after asking AWS to disassociate the transit gateway VPC
    attachment and the transit gateway's default route table we sleep in a
    loop until the disassociation is complete.

--- transitgateway.tf
+++ transitgateway.tf
@@ -26,7 +26,37 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "assessment" {
   vpc_id             = aws_vpc.assessment.id
 }
 
+# Break the association between the transit gateway VPC attachment and
+# the default transit gateway route table.
+#
+# It would be nice not to have to use the Terraform escape hatch for
+# this, but Terraform just doesn't support this sort of cross-account
+# foo right now.
+resource "null_resource" "break_association_with_default_route_table" {
+  # Require that the transit gateway VPC attachment is created before
+  # breaking the association.
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.assessment,
+  ]
+
+  provisioner "local-exec" {
+    when = create
+    # This command asks AWS to disassociate the default route table
+    # from our transit gateway attachment, then loops until the
+    # disassociation is complete.
+    command = "aws --profile cool-sharedservices-provisionaccount ec2 disassociate-transit-gateway-route-table --transit-gateway-route-table-id ${local.transit_gateway_default_route_table_id} --transit-gateway-attachment-id ${aws_ec2_transit_gateway_vpc_attachment.assessment.id} && while aws --profile cool-sharedservices-provisionaccount ec2 get-transit-gateway-route-table-associations --transit-gateway-route-table-id ${local.transit_gateway_default_route_table_id} | grep --quiet ${aws_vpc.assessment.id}; do sleep 5s; done"
+  }
+
+  triggers = {
+    tgw_default_route_table_id = local.transit_gateway_default_route_table_id
+    tgw_vpc_attachment_id      = aws_ec2_transit_gateway_vpc_attachment.assessment.id
+  }
+}
+
 resource "aws_ec2_transit_gateway_route_table_association" "association" {
+  depends_on = [
+    null_resource.break_association_with_default_route_table,
+  ]
   provider = aws.provisionsharedservices
 
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.assessment.id
