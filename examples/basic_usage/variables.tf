7a58c02f1f04f5515878dca4eaa4673287b11e5e
Dave Redmin <david.redmin@beta.dhs.gov>
2020-10-22 11:34:33 -0400

    Add a variables file

---
+++ examples/basic_usage/variables.tf
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------------
+# Required parameters
+#
+# You must provide a value for each of these parameters.
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Optional parameters
+#
+# These parameters have reasonable defaults.
+# ------------------------------------------------------------------------------
+
+variable "ami_owner_account_id" {
+  description = "The ID of the AWS account that owns the AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
+  default     = "self"
+}
+
+variable "aws_availability_zone" {
+  type        = string
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
+  default     = "a"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  default     = "us-east-1"
+}
+
+variable "tf_role_arn" {
+  type        = string
+  description = "The ARN of the role that can terraform non-specialized resources."
+}
