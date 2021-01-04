6a449eb5c2e66fda3eca0893a4960cea96b26a3e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-04 12:42:46 -0500

    Add a cloud-init shell script to ensure mounting of the EFS share
    Even with the changes in commit
    638f1824d588b0631048ba3437e72d882da1349c, it is still possible for an
    instance so slip through the cracks and boot without mounting the EFS
    share.  This is because Terraform has no way to distinguish between
    the "resource created" and "resource created and ready for use"
    states.  The share can be created, but not become available for
    attachment until _after_ the instance has already booted up and
    automounted the mounts listed in /etc/fstab.

---
+++ cloud-init/mount-efs-share.tpl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# This is a Terraform template file, and the mount_point variable is
+# passed in via the templatefile().
+#
+# shellcheck disable=SC2154
+until findmnt "${mount_point}"
+do
+    sleep 2
+    echo "Attempting to mount EFS share."
+    mount --all
+done
--- kali_cloud_init.tf
+++ kali_cloud_init.tf
@@ -10,6 +10,7 @@ data "cloudinit_config" "kali_cloud_init_tasks" {
   # filenames will also be used as a filename in the scripts
   # directory.
 
+  # Create an fstab entry for the EFS share
   part {
     content = templatefile(
       "${path.module}/cloud-init/efs-mount.tpl.yml", {
@@ -19,6 +20,21 @@ data "cloudinit_config" "kali_cloud_init_tasks" {
     })
     content_type = "text/cloud-config"
     filename     = "efs_mount.yml"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # This shell script loops until the EFS share is mounted.  We do
+  # make the instance depend on the EFS share in the Terraform code,
+  # but it is still possible for an instance to boot up without
+  # mounting the share.  See this issue comment for more details:
+  # https://github.com/cisagov/cool-assessment-terraform/issues/85#issuecomment-754052796
+  part {
+    content = templatefile(
+      "${path.module}/cloud-init/mount-efs-share.tpl.sh", {
+        mount_point = "/share"
+    })
+    content_type = "text/x-shellscript"
+    filename     = "mount-efs-share.sh"
     merge_type   = "list(append)+dict(recurse_array)+str()"
   }
 }
