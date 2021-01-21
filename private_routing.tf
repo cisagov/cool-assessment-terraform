597a54164eb3dfbdd6e46e0bc1c8c70c0d50e46f
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-21 14:33:20 -0500

    Revert "Remove NAT gateway and associated route"
    This reverts commit 6b091f01e9fe8427a9a5fc9a4df3d0152af4762f.
    
    I realized that the guacamole instance still needs the NAT gateway and
    external web access in order to download the Docker images used in the
    guacamole docker composition.

--- private_routing.tf
+++ private_routing.tf
@@ -41,6 +41,16 @@ resource "aws_vpc_endpoint_route_table_association" "s3_private" {
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
 }
 
+# Route all external (outside this VPC and outside the COOL) traffic
+# through the NAT gateway
+resource "aws_route" "external_private" {
+  provider = aws.provisionassessment
+
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
 # Associate the routing table with the subnets
 resource "aws_route_table_association" "private_route_table_associations" {
   provider = aws.provisionassessment
