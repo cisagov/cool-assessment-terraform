5c087eaabcdb73f690657cc63ef162074622d9c3
Shane Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-22 10:37:48 -0500

    Add issue link to a comment
    Co-authored-by: Hillary <hillary.jeffrey@trio.dhs.gov>

--- sts_endpoint_sg_rules.tf
+++ sts_endpoint_sg_rules.tf
@@ -14,7 +14,8 @@ resource "aws_security_group_rule" "ingress_from_desktop_gw_to_sts_via_https" {
 #
 # TODO - Only the Nessus instances require this access.  We could
 # avoid giving it to the other instance types by creating a separate
-# security group for Nessus instances.  See #95 for more details.
+# security group for Nessus instances.  See #95 for more details:
+# https://github.com/cisagov/cool-assessment-terraform/issues/95
 resource "aws_security_group_rule" "ingress_from_operations_to_sts_via_https" {
   provider = aws.provisionassessment
 
