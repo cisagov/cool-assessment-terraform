24d3c09fbb2c8b2c3d7f8cf2b14dc0e3584c2716
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-21 22:44:49 -0500

    Allow the operations subnet to use the STS VPC endpoint

--- sts_endpoint_sg_rules.tf
+++ sts_endpoint_sg_rules.tf
@@ -9,3 +9,19 @@ resource "aws_security_group_rule" "ingress_from_desktop_gw_to_sts_via_https" {
   from_port                = 443
   to_port                  = 443
 }
+
+# Allow ingress via HTTPS from the operations security group
+#
+# TODO - Only the Nessus instances require this access.  We could
+# avoid giving it to the other instance types by creating a separate
+# security group for Nessus instances.  See #95 for more details.
+resource "aws_security_group_rule" "ingress_from_operations_to_sts_via_https" {
+  provider = aws.provisionassessment
+
+  security_group_id        = aws_security_group.sts.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.operations.id
+  from_port                = 443
+  to_port                  = 443
+}
