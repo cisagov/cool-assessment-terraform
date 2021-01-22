5a40a3a80152600df6cad2d489a47f27eae17711
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-22 10:42:39 -0500

    Update comment to agree with reality
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- private_routing.tf
+++ private_routing.tf
@@ -12,11 +12,14 @@
 # defined in operations_routing.tf.
 # -------------------------------------------------------------------------------
 
-# The private subnets can all share a single routing table.  Normally
-# we would assign each subnet its own NAT gateway, which would require
-# separate routing tables, but in this case we do not create any NAT
-# gateways since nothing in the private subnets requires access to the
-# internet outside the VPC.
+# The private subnets can all share a single routing table.
+#
+# Normally we would assign each public subnet its own NAT gateway and
+# route external traffic from each private subnet to the NAT gateway
+# in the public subnet that resides in the same AZ, and this would
+# obviously require separate routing tables.  However, in this case we
+# only create a single NAT gateway in the one operations subnet that
+# is shared by all private subnets.
 resource "aws_route_table" "private_route_table" {
   provider = aws.provisionassessment
 
