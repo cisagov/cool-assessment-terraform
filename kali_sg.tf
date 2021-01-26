83daa7bcddf1538a0d602768d0f12ef056a6f2bb
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-26 14:18:47 -0500

    Reference the Kali SG instead of CIDR blocks in the PenTest Portal SG

--- kali_sg.tf
+++ kali_sg.tf
@@ -12,26 +12,29 @@ resource "aws_security_group" "kali" {
   )
 }
 
-# Allow egress to teamservers via port 993 (IMAP over TLS/SSL)
-resource "aws_security_group_rule" "kali_egress_to_teamserver_via_imaps" {
+# Allow egress to Pentest Portal instances via ports 443 and 8080
+resource "aws_security_group_rule" "kali_egress_to_pentestportal_via_web" {
+  for_each = toset(["443", "8080"])
   provider = aws.provisionassessment
 
   security_group_id        = aws_security_group.kali.id
   type                     = "egress"
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.teamserver.id
-  from_port                = 993
-  to_port                  = 993
+  source_security_group_id = aws_security_group.pentestportal.id
+  from_port                = each.key
+  to_port                  = each.key
 }
 
-# Allow egress to teamservers via port 50050 (Cobalt Strike)
-resource "aws_security_group_rule" "kali_egress_to_teamserver_via_cs" {
+# Allow egress to teamservers via ports 993 and 50050 (IMAP over
+# TLS/SSL and Cobalt Strike, respectively)
+resource "aws_security_group_rule" "kali_egress_to_teamserver_via_imaps_and_cs" {
+  for_each = toset(["993", "50050"])
   provider = aws.provisionassessment
 
   security_group_id        = aws_security_group.kali.id
   type                     = "egress"
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.teamserver.id
-  from_port                = 50050
-  to_port                  = 50050
+  from_port                = each.key
+  to_port                  = each.key
 }
