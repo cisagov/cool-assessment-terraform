3c774aa9c1fc298b9701125711b17612c6e357b2
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-26 14:26:02 -0500

    Combine two SG rules into one using a for_each clause

--- teamserver_sg.tf
+++ teamserver_sg.tf
@@ -12,26 +12,16 @@ resource "aws_security_group" "teamserver" {
   )
 }
 
-# Allow ingress from Kali instances via port 993 (IMAP over TLS/SSL)
-resource "aws_security_group_rule" "teamserver_ingress_from_kali_via_imaps" {
+# Allow ingress from Kali instances via ports 993 and 50050 (IMAP over
+# TLS/SSL and Cobalt Strike, respectively)
+resource "aws_security_group_rule" "teamserver_ingress_from_kali_via_imaps_and_cs" {
+  for_each = toset(["993", "50050"])
   provider = aws.provisionassessment
 
   security_group_id        = aws_security_group.teamserver.id
   type                     = "ingress"
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.kali.id
-  from_port                = 993
-  to_port                  = 993
-}
-
-# Allow ingress from Kali instances via port 50050 (Cobalt Strike)
-resource "aws_security_group_rule" "teamserver_ingress_from_kali_via_cs" {
-  provider = aws.provisionassessment
-
-  security_group_id        = aws_security_group.teamserver.id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.kali.id
-  from_port                = 50050
-  to_port                  = 50050
+  from_port                = each.key
+  to_port                  = each.key
 }
