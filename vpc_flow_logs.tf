33bc3888c759a2b2cea6d5a9e9edd60849d99f58
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-08 14:26:50 -0500

    Pin the VPC flow logs module we are using
    This module dropped support for TF 0.12 in version 2.1.0.

--- vpc_flow_logs.tf
+++ vpc_flow_logs.tf
@@ -3,6 +3,8 @@
 #-------------------------------------------------------------------------------
 module "vpc_flow_logs" {
   source = "trussworks/vpc-flow-logs/aws"
+  # Version 2.1.0 dropped support for TF 0.12
+  version = ">=2.0.0, <2.1.0"
   providers = {
     aws = aws.provisionassessment
   }
