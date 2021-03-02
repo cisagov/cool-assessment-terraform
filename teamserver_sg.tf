f4ea18194d66f344371bfd6a05796b139299b985
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-03-02 09:58:23 -0500

    Modify the security group for each instance type to open only the specified ports
    These are the ports specified to be opened for that instance type in the inbound_ports_allowed variable.

--- teamserver_sg.tf
+++ teamserver_sg.tf
@@ -25,3 +25,19 @@ resource "aws_security_group_rule" "teamserver_ingress_from_kali_via_imaps_and_c
   from_port                = each.key
   to_port                  = each.key
 }
+
+# Allow ingress from anywhere via the allowed ports
+resource "aws_security_group_rule" "ingress_from_anywhere_to_teamserver_via_allowed_ports" {
+  provider = aws.provisionassessment
+  # for_each will only accept a map or a list of strings, so we have
+  # to do a little finagling to get the list of port objects into an
+  # acceptable form.
+  for_each = { for index, d in var.inbound_ports_allowed["teamserver"] : index => d }
+
+  security_group_id = aws_security_group.teamserver.id
+  type              = "ingress"
+  protocol          = each.value["protocol"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = each.value["from_port"]
+  to_port           = each.value["to_port"]
+}
