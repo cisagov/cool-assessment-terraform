b1ad010cda4cc9eb08f71c5abecf87d6647ba7d0
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-05-24 11:58:19 -0400

    Give teamserver its own cloud init configuration
    This cloud-init configuration allows for mounting the EFS share and
    installing the appropriate certificates.

---
+++ teamserver_cloud_init.tf
@@ -0,0 +1,53 @@
+# cloud-init commands for configuring teamserver instances
+
+data "cloudinit_config" "teamserver_cloud_init_tasks" {
+  gzip          = true
+  base64_encode = true
+
+  # Note: The filename parameters in each part below are only used to
+  # name the mime-parts of the user-data.  They do not affect the
+  # final name for the templates. For any x-shellscript parts, the
+  # filenames will also be used as a filename in the scripts
+  # directory.
+
+  # Create an fstab entry for the EFS share
+  part {
+    content = templatefile(
+      "${path.module}/cloud-init/efs-mount.tpl.yml", {
+        # Just mount the EFS mount target in the first private subnet
+        efs_id      = aws_efs_mount_target.target[var.private_subnet_cidr_blocks[0]].file_system_id
+        mount_point = "/share"
+    })
+    content_type = "text/cloud-config"
+    filename     = "efs_mount.yml"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # This shell script loops until the EFS share is mounted.  We do
+  # make the instance depend on the EFS share in the Terraform code,
+  # but it is still possible for an instance to boot up without
+  # mounting the share.  See this issue comment for more details:
+  # https://github.com/cisagov/cool-assessment-terraform/issues/85#issuecomment-754052796
+  part {
+    content = templatefile(
+      "${path.module}/cloud-init/mount-efs-share.tpl.sh", {
+        mount_point = "/share"
+    })
+    content_type = "text/x-shellscript"
+    filename     = "mount-efs-share.sh"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # Install certificates
+  part {
+    filename     = "install-certificates.py"
+    content_type = "text/x-shellscript"
+    content = templatefile(
+      "${path.module}/cloud-init/install-certificates.py", {
+        aws_region         = var.aws_region
+        cert_bucket_name   = var.cert_bucket_name
+        cert_read_role_arn = module.teamserver_certreadrole.role.arn
+        server_fqdn        = "*.${var.email_sending_domain}"
+    })
+  }
+}
