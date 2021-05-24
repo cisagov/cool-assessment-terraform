006ce8a0cc679e8a42a6b1b5adaec4410ebd48df
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-05-24 11:56:08 -0400

    Give teamserver IAM permissions to pull its certificate from a bucket
    This necessitates the addition of a cisagov/cert-read-role-tf-module.

--- teamserver_iam.tf
+++ teamserver_iam.tf
@@ -1,5 +1,18 @@
+# Create a role that allows the instance to read its certs from S3.
+module "teamserver_certreadrole" {
+  source = "github.com/cisagov/cert-read-role-tf-module"
+
+  providers = {
+    aws = aws.provisioncertreadrole
+  }
+
+  account_ids      = [local.assessment_account_id]
+  cert_bucket_name = var.cert_bucket_name
+  hostname         = "*.${var.email_sending_domain}"
+}
+
 # Create the IAM instance profile for the Teamserver EC2 server
-# instances
+# instance
 
 # The instance profile to be used
 resource "aws_iam_instance_profile" "teamserver" {
@@ -13,10 +26,16 @@ resource "aws_iam_instance_profile" "teamserver" {
 resource "aws_iam_role" "teamserver_instance_role" {
   provider = aws.provisionassessment
 
-  name = "teamserver_instance_role_${terraform.workspace}"
-  # We can just reuse the kali assume role policy here.  If we created
-  # a teamserver-specific one it would be identical.
-  assume_role_policy = data.aws_iam_policy_document.kali_assume_role_policy_doc.json
+  name               = "teamserver_instance_role_${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.teamserver_assume_role_policy_doc.json
+}
+
+resource "aws_iam_role_policy" "teamserver_assume_delegated_role_policy" {
+  provider = aws.provisionassessment
+
+  name   = "assume_delegated_role_policy"
+  role   = aws_iam_role.teamserver_instance_role.id
+  policy = data.aws_iam_policy_document.teamserver_assume_delegated_role_policy_doc.json
 }
 
 # Attach the CloudWatch Agent policy to this role as well
@@ -35,13 +54,61 @@ resource "aws_iam_role_policy_attachment" "ssm_agent_policy_attachment_teamserve
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Attach a policy that allows the Teamserver instances to mount and
-# write to the EFS
+# Attach a policy that allows the Teamserver instances to mount and write to
+# the EFS
 resource "aws_iam_role_policy_attachment" "efs_mount_policy_attachment_teamserver" {
   provider = aws.provisionassessment
 
-  role = aws_iam_role.teamserver_instance_role.id
-  # We can just reuse the kali policy here.  If we created a
-  # teamserver-specific one it would be identical.
-  policy_arn = aws_iam_policy.kali_efs_mount_policy.arn
+  role       = aws_iam_role.teamserver_instance_role.id
+  policy_arn = aws_iam_policy.teamserver_efs_mount_policy.arn
+}
+
+################################
+# Define the role policies below
+################################
+
+data "aws_iam_policy_document" "teamserver_assume_role_policy_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# The policy doc that allows the teamserver instances to mount and
+# write to the EFS
+data "aws_iam_policy_document" "teamserver_efs_mount_policy_doc" {
+  statement {
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientRootAccess",
+      "elasticfilesystem:ClientWrite"
+    ]
+    effect = "Allow"
+    resources = [
+      # Allow mounting of the EFS mount target in the first private
+      # subnet
+      aws_efs_mount_target.target[var.private_subnet_cidr_blocks[0]].file_system_arn
+    ]
+  }
+}
+resource "aws_iam_policy" "teamserver_efs_mount_policy" {
+  provider = aws.provisionassessment
+
+  policy = data.aws_iam_policy_document.teamserver_efs_mount_policy_doc.json
+}
+
+# Allow the teamserver instance to assume the necessary role to read
+# the teamserver certificates from an S3 bucket.
+data "aws_iam_policy_document" "teamserver_assume_delegated_role_policy_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    resources = [
+      module.teamserver_certreadrole.role.arn,
+    ]
+  }
 }
