ae8205c45c8f098216c24a4d233e22f821ddef71
dav3r <david.redmin@trio.dhs.gov>
2021-06-07 10:52:39 -0400

    Alphabetically sort keys
    Co-authored-by: Nick M. <50747025+mcdonnnj@users.noreply.github.com>

--- providers.tf
+++ providers.tf
@@ -3,10 +3,10 @@
 # assume the roles required to access remote state in the Terraform backend.
 
 provider "aws" {
-  region = var.aws_region
   default_tags {
     tags = var.tags
   }
+  region = var.aws_region
 }
 
 provider "aws" {
