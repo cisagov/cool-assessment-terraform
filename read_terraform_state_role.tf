39277b0bdd216f4474bb481b60d900cb48ba5383
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-08-17 12:10:51 -0400

    Do not create the terraform state read role assume role
    This makes use of the new create_assume_role variable from
    cisagov/terraform-state-read-role-tf-module#7.

--- read_terraform_state_role.tf
+++ read_terraform_state_role.tf
@@ -5,16 +5,23 @@
 # ------------------------------------------------------------------------------
 
 module "read_terraform_state" {
-  source = "github.com/cisagov/terraform-state-read-role-tf-module"
+  source = "github.com/cisagov/terraform-state-read-role-tf-module?ref=improvement%2Fmake-assume-role-creation-optional"
 
   providers = {
     aws = aws.provisionterraform
     # This provider is poorly named.  It is a provider that is allowed
     # to create IAM resources in the account(s) listed in account_ids.
+    # In any event, this provider is not used here at all since the
+    # module is configured with create_assume_role equal to false.  We
+    # do have to put something there are a placeholder, though.
     aws.users = aws.provisionassessment
   }
 
   account_ids = [local.assessment_account_id]
+  # We don't want to create the assume role, since in this case the
+  # Terraformer instance role is given permission to assume this role
+  # directly in terraformer_iam.tf.
+  create_assume_role = false
   # Note that the replace() function replaces "env0 (Staging)", for
   # example, with env0-Staging when it occurs at the end of the string
   role_name                   = format(var.read_terraform_state_role_name, replace(var.assessment_account_name, "/ \\((?P<env_type>[[:alnum:]]*)\\)$/", "-$env_type"))
