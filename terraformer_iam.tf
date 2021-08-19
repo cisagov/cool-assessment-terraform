e615aed0e709d1991af85f7caa68f198ce86ea7a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-08-18 23:15:00 -0400

    Give the Terraformer instance role permission to assume the VNC Parameter Store read-only role
    This is so the cloud-init script that writes out the AWS config file
    can assume this role to read the VNC user's username.

--- terraformer_iam.tf
+++ terraformer_iam.tf
@@ -58,6 +58,7 @@ data "aws_iam_policy_document" "terraformer_assume_delegated_role_policy_doc" {
       aws_iam_role.terraformer_role.arn,
       module.read_terraform_state.role.arn,
       data.terraform_remote_state.master.outputs.organizationsreadonly_role.arn,
+      aws_iam_role.vnc_parameterstorereadonly_role.arn,
     ]
   }
 }
