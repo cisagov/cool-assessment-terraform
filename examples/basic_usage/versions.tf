e24959a1413272637daa6b54c897fdb9e56a5b63
David Redmin <david.redmin@trio.dhs.gov>
2021-08-30 16:59:45 -0400

    Make example versions.tf consistent with the module's versions.tf file
    Co-Authored-By: Nick M. <mcdonnnj@users.noreply.github.com>

--- examples/basic_usage/versions.tf
+++ examples/basic_usage/versions.tf
@@ -1,8 +1,17 @@
 terraform {
+  # We want to hold off on 0.14 or higher until we have tested it.
+  required_version = "~> 0.13.0"
+
+  # If you use any other providers you should also pin them to the
+  # major version currently being used.  This practice will help us
+  # avoid unwelcome surprises.
   required_providers {
+    # Version 3.38.0 of the Terraform AWS provider is the first
+    # version to support default tags.
+    # https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.38"
     }
   }
-  required_version = "~> 0.13.0"
 }
