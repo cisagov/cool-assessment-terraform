4b926851dd8f8d5123345ff6523bd61fe7cbaf33
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-09-20 22:46:09 -0400

    Make Terraformer instances mount the EFS share
    This is something that the assessors have asked for.

--- terraformer_cloud_init.tf
+++ terraformer_cloud_init.tf
@@ -10,6 +10,34 @@ data "cloudinit_config" "terraformer_cloud_init_tasks" {
   # filenames will also be used as a filename in the scripts
   # directory.
 
+  # Create an fstab entry for the EFS share
+  part {
+    content = templatefile(
+      "${path.module}/cloud-init/efs-mount.tpl.yml", {
+        # Just mount the EFS mount target in the first private subnet
+        efs_id      = aws_efs_mount_target.target[var.private_subnet_cidr_blocks[0]].file_system_id
+        mount_point = "/share"
+    })
+    content_type = "text/cloud-config"
+    filename     = "efs_mount.yml"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
+  # This shell script loops until the EFS share is mounted.  We do
+  # make the instance depend on the EFS share in the Terraform code,
+  # but it is still possible for an instance to boot up without
+  # mounting the share.  See this issue comment for more details:
+  # https://github.com/cisagov/cool-assessment-terraform/issues/85#issuecomment-754052796
+  part {
+    content = templatefile(
+      "${path.module}/cloud-init/mount-efs-share.tpl.sh", {
+        mount_point = "/share"
+    })
+    content_type = "text/x-shellscript"
+    filename     = "mount-efs-share.sh"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
+
   # Create a credentials file for the VNC user that can be used to
   # configure the AWS CLI to assume the Terraformer role by default
   # and other roles by selecting the correct profile.  For details,
@@ -51,5 +79,6 @@ data "cloudinit_config" "terraformer_cloud_init_tasks" {
     })
     content_type = "text/x-shellscript"
     filename     = "write-aws-config.sh"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
 }
