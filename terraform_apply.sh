b237a61695dafb78dda873ae1403fb40fbe9ed9a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-04-01 15:22:38 -0400

    Add scripts for creating and destroying assessment environments
    This is necessary because the CloudWatch alarm resources associated
    with the EC2 instances are created using for_each expressions that are
    dynamic-ish.

---
+++ terraform_apply.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# A script for creating assessment environments using
+# cisagov/cooll-assessment-terraform.
+#
+# This is necessary because the CloudWatch alarm resources associated
+# with the EC2 instances are created using for_each expressions that
+# are dynamic-ish.
+#
+# Examples:
+# - See what would be created:
+#   $ AWS_PROFILE=cool-user AWS_SHARED_CREDENTIALS_FILE=~/.aws/production_credentials AWS_DEFAULT_REGION=us-east-1 ./terraform_apply.sh -var-file=envX-production.tfvars
+# - Destroy it!  (You will not be prompted.)
+#   $ AWS_PROFILE=cool-user AWS_SHARED_CREDENTIALS_FILE=~/.aws/production_credentials AWS_DEFAULT_REGION=us-east-1 ./terraform_apply.sh -auto-apply -var-file=envX-production.tfvars
+
+# Export some environment variables that we want the terraform child
+# processes to inherit.
+export AWS_PROFILE
+export AWS_SHARED_CREDENTIALS_FILE
+export AWS_DEFAULT_REGION
+
+# Perform a targeted apply to create the EC2 instances, then create
+# everything else.  This is a workaround for the dynamic-ish for_each
+# expressions mentinoed above.
+terraform apply "${@}" \
+  -target=aws_iam_role_policy_attachment.provisionassessment_policy_attachment \
+  && terraform apply "${@}" \
+    -target=aws_instance.assessorportal \
+    -target=aws_instance.debiandesktop \
+    -target=aws_instance.gophish \
+    -target=aws_instance.guacamole \
+    -target=aws_instance.kali \
+    -target=aws_instance.nessus \
+    -target=aws_instance.pentestportal \
+    -target=aws_instance.samba \
+    -target=aws_instance.teamserver \
+    -target=aws_instance.terraformer \
+    -target=aws_instance.windows \
+  && terraform apply "${@}"
---
+++ terraform_destroy_all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# A script for destroying assessment environments created using
+# cisagov/cooll-assessment-terraform.
+#
+# This is necessary because the CloudWatch alarm resources associated
+# with the EC2 instances are created using for_each expressions that
+# are dynamic-ish.
+#
+# Examples:
+# - See what would be destroyed:
+#   $ AWS_PROFILE=cool-user AWS_SHARED_CREDENTIALS_FILE=~/.aws/production_credentials AWS_DEFAULT_REGION=us-east-1 ./terraform_destroy_all.sh -var-file=envX-production.tfvars
+# - Destroy it!  (You will not be prompted.)
+#   $ AWS_PROFILE=cool-user AWS_SHARED_CREDENTIALS_FILE=~/.aws/production_credentials AWS_DEFAULT_REGION=us-east-1 ./terraform_destroy_all.sh -auto-apply -var-file=envX-production.tfvars
+
+# Export some environment variables that we want the terraform child
+# processes to inherit.
+export AWS_PROFILE
+export AWS_SHARED_CREDENTIALS_FILE
+export AWS_DEFAULT_REGION
+
+# Create a list of resources to target from the terraform state and
+# then pass them to terraform destroy.
+terraform state list | sed "s/^/-target='/;s/$/'/" | xargs terraform destroy "$@"
