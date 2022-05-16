8c6e5c16ce0ebca35ce17c73b746abd998d29272
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-05-16 15:54:55 -0400

    Make comment agree with the one in provisionssmsessionmanager_policy.tf
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- provisionssmsessionmanager_policy_attachment.tf
+++ provisionssmsessionmanager_policy_attachment.tf
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------------------
-# Attach to the ProvisionAccount role the IAM policy that allows
-# provisioning of the SSM Document resource and set up SSM session
-# logging in this account.
+# Attach to the ProvisionAccount role the IAM policy that allows all
+# of the permissions necessary to provision the SSM Document resource
+# and set up SSM session logging in this account.
 # ------------------------------------------------------------------------------
 
 resource "aws_iam_role_policy_attachment" "provisionssmsessionmanager_policy_attachment" {
