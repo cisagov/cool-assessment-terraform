61e7f2703fb9ddc19bc524032156de7af0a11ebe
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-12-20 12:24:32 -0500

    Rework the way the Kali instance is allowed to assume delegated roles
    The new way is consistent with what we did for the Terraformer
    instance.

--- kali_iam.tf
+++ kali_iam.tf
@@ -16,6 +16,14 @@ resource "aws_iam_role" "kali_instance_role" {
   assume_role_policy = data.aws_iam_policy_document.ec2_service_assume_role_doc.json
 }
 
+resource "aws_iam_role_policy" "kali_assume_delegated_role_policy" {
+  provider = aws.provisionassessment
+
+  name   = "kali_assume_delegated_role_policy"
+  role   = aws_iam_role.kali_instance_role.id
+  policy = data.aws_iam_policy_document.kali_assume_delegated_role_policy_doc.json
+}
+
 # Attach the CloudWatch Agent policy to this role as well
 resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment_kali" {
   provider = aws.provisionassessment
@@ -41,11 +49,22 @@ resource "aws_iam_role_policy_attachment" "efs_mount_policy_attachment_kali" {
   policy_arn = aws_iam_policy.efs_mount_policy.arn
 }
 
-# Attach the policy that allows the Kali instances to assume the role in the
-# Shared Services account that allows writing to the assessment findings bucket
-resource "aws_iam_role_policy_attachment" "assessmentfindingsbucketwrite_attachment_kali" {
-  provider = aws.provisionassessment
+################################
+# Define the role policies below
+################################
 
-  role       = aws_iam_role.kali_instance_role.id
-  policy_arn = aws_iam_policy.assume_assessmentfindingsbucketwrite_sharedservices_policy.arn
+# Allow the Kali instance to assume the necessary roles to perform its
+# function.
+data "aws_iam_policy_document" "kali_assume_delegated_role_policy_doc" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+    effect = "Allow"
+    resources = [
+      aws_iam_role.gucamole_parameterstorereadonly_role.arn,
+      data.terraform_remote_state.sharedservices.outputs.assessment_findings_write_role.arn,
+    ]
+  }
 }
