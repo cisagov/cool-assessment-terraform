cf26fb4e00adf77b3118bf9626cebeebe5c5e398
Miguel Rios <miguel.rios@trio.dhs.gov>
2022-12-23 15:32:39 -0500

    Define Egress-Assess security group and rules

---
+++ egressassess_sg.tf
@@ -0,0 +1,41 @@
+# Security group for the Egress-Assess instances
+resource "aws_security_group" "egressassess" {
+  provider = aws.provisionassessment
+
+  vpc_id = aws_vpc.assessment.id
+
+  tags = {
+    "Name" = "Egress-Assess"
+  }
+}
+
+# Allow egress to anywhere via HTTP and HTTPS
+#
+# For: Package downloads and updates
+resource "aws_security_group_rule" "egressassess_egress_to_anywhere_via_http_and_https" {
+  for_each = toset(["80", "443"])
+  provider = aws.provisionassessment
+
+  security_group_id = aws_security_group.egressassess.id
+  type              = "egress"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = each.key
+  to_port           = each.key
+}
+
+# Allow ingress from anywhere via the allowed ports
+resource "aws_security_group_rule" "ingress_from_anywhere_to_egressassess_via_allowed_ports" {
+  provider = aws.provisionassessment
+  # for_each will only accept a map or a list of strings, so we have
+  # to do a little finagling to get the list of port objects into an
+  # acceptable form.
+  for_each = { for d in var.inbound_ports_allowed["egressassess"] : format("%s_%d_%d", d.protocol, d.from_port, d.to_port) => d }
+
+  security_group_id = aws_security_group.egressassess.id
+  type              = "ingress"
+  protocol          = each.value["protocol"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = each.value["from_port"]
+  to_port           = each.value["to_port"]
+}
