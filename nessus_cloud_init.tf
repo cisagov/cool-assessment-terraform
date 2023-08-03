0ceee4aaa44cbe68132ed8ba078d890e55f0a719
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-08-03 09:44:27 -0400

    Set Nessus web server port in nessus-setup.sh
    Since we already have a Nessus-related cloud-init script, it makes sense to add this functionality to it.

--- nessus_cloud_init.tf
+++ nessus_cloud_init.tf
@@ -37,6 +37,7 @@ data "cloudinit_config" "nessus_cloud_init_tasks" {
       "${path.module}/cloud-init/nessus-setup.sh", {
         aws_region                    = var.aws_region
         nessus_activation_code        = var.nessus_activation_codes[count.index]
+        nessus_web_server_port        = var.nessus_web_server_port
         ssm_key_nessus_admin_password = var.ssm_key_nessus_admin_password
         ssm_key_nessus_admin_username = var.ssm_key_nessus_admin_username
         ssm_nessus_read_role_arn      = aws_iam_role.nessus_parameterstorereadonly_role.arn
