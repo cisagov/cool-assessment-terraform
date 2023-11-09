45869c35e63b1e602f074c19d4f3d34278404dd6
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-11-09 12:00:05 -0500

    Alphabetize resource attributes in variables.tf

--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@
 # ------------------------------------------------------------------------------
 
 variable "subnet_id" {
-  type        = string
   description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)."
+  type        = string
 }
 
 # ------------------------------------------------------------------------------
@@ -15,19 +15,19 @@ variable "subnet_id" {
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
   default     = "self"
+  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
+  type        = string
 }
 
 variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
   default     = "a"
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
+  type        = string
 }
 
 variable "aws_region" {
-  type        = string
-  description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  type        = string
 }
