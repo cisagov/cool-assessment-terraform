3f1c503b5fdcc4a35804636f777db463c12bcf31
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-11-09 12:02:10 -0500

    Alphabetize resource attributes in examples/basic_usage/

--- examples/basic_usage/versions.tf
+++ examples/basic_usage/versions.tf
@@ -1,7 +1,4 @@
 terraform {
-  # We want to hold off on 1.1 or higher until we have tested it.
-  required_version = "~> 1.0"
-
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
   # avoid unwelcome surprises.
@@ -20,4 +17,7 @@ terraform {
       version = "~> 4.9"
     }
   }
+
+  # We want to hold off on 1.1 or higher until we have tested it.
+  required_version = "~> 1.0"
 }
