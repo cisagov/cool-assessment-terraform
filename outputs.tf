8bbce64442a8541bebde6852b67b888422d67b1f
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-11-09 11:59:20 -0500

    Alphabetize resource attributes in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "arn" {
-  value       = aws_instance.example.arn
   description = "The EC2 instance ARN."
+  value       = aws_instance.example.arn
 }
 
 output "availability_zone" {
-  value       = aws_instance.example.availability_zone
   description = "The AZ where the EC2 instance is deployed."
+  value       = aws_instance.example.availability_zone
 }
 
 output "id" {
-  value       = aws_instance.example.id
   description = "The EC2 instance ID."
+  value       = aws_instance.example.id
 }
 
 output "private_ip" {
-  value       = aws_instance.example.private_ip
   description = "The private IP of the EC2 instance."
+  value       = aws_instance.example.private_ip
 }
 
 output "subnet_id" {
-  value       = aws_instance.example.subnet_id
   description = "The ID of the subnet where the EC2 instance is deployed."
+  value       = aws_instance.example.subnet_id
 }
