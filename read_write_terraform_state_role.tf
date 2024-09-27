00c07d7aed82f559e72770e18b1be6006f7335a8
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-09-27 13:33:25 -0400

    Add an IAM policy and role that allows reading and writing to the Terraform state
    The intent of this role is that selected users could assume it to
    redeploy their own COOL environments.
    
    Note that this role is workspace-specific, so if access is granted
    then users can only defile their own COOL environment.

---
+++ read_write_terraform_state_role.tf
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------------------
+# Create the IAM policy and role that allows read-write access to the
+# Terraform state for this project in the S3 bucket where Terraform
+# remote state is stored.
+# ------------------------------------------------------------------------------
+
+module "read_write_terraform_state" {
+  source = "github.com/cisagov/terraform-state-read-role-tf-module?ref=feature%2Fadd-option-for-write-permission"
+
+  providers = {
+    aws = aws.provisionterraform
+    # This provider is poorly named.  It is a provider that is allowed
+    # to create IAM resources in the account(s) listed in account_ids.
+    # In any event, this provider is not used here at all since the
+    # module is configured with create_assume_role equal to false.  We
+    # do have to put something there as a placeholder, though.
+    aws.users = aws.provisionassessment
+  }
+
+  # The intent of this role is to allow selected IAM users to redeploy
+  # this Terraform workspace.
+  account_ids        = [local.users_account_id]
+  create_assume_role = false
+  read_only          = false
+  # Note that the replace() function replaces "env0 (Staging)", for
+  # example, with env0-Staging when it occurs at the end of the string
+  role_name                   = format(var.read_write_terraform_state_role_name, replace(var.assessment_account_name, "/ \\((?P<env_type>[[:alnum:]]*)\\)$/", "-$env_type"))
+  terraform_state_bucket_name = "cisa-cool-terraform-state"
+  terraform_state_path        = "cool-assessment-terraform/terraform.tfstate"
+  terraform_workspace         = local.assessment_workspace_name
+}
