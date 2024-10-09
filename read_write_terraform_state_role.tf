e0911be5eb8d5c2efa58473d6d341d2a57d436f8
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-10-09 14:14:44 -0400

    Temporarily use a non-default branch of cisagov/terraform-state-read-role-tf-module
    This is being done for testing purposes, and this change can be
    reverted once cisagov/terraform-state-read-role-tf-module#39 is
    merged.
    
    Also update the module arguments as necessary to match the code in the
    non-default branch.

--- read_write_terraform_state_role.tf
+++ read_write_terraform_state_role.tf
@@ -5,7 +5,7 @@
 # ------------------------------------------------------------------------------
 
 module "read_write_terraform_state" {
-  source = "github.com/cisagov/terraform-state-read-role-tf-module"
+  source = "github.com/cisagov/terraform-state-read-role-tf-module?ref=bugfix%2Fadd-permissions-to-write-dynamodb-lock-table"
 
   providers = {
     aws = aws.provisionterraform
@@ -19,9 +19,11 @@ module "read_write_terraform_state" {
 
   # The intent of this role is to allow selected IAM users to redeploy
   # this Terraform workspace.
-  account_ids        = [local.users_account_id]
-  create_assume_role = false
-  read_only          = false
+  account_ids         = [local.users_account_id]
+  create_assume_role  = false
+  lock_db_policy_name = format("TerraformLockDbPolicy-%s", replace(var.assessment_account_name, "/ \\((?P<env_type>[[:alnum:]]*)\\)$/", "-$env_type"))
+  lock_db_table_arn   = data.terraform_remote_state.terraform.outputs.state_lock_table.arn
+  read_only           = false
   # Note that the replace() function replaces "env0 (Staging)", for
   # example, with env0-Staging when it occurs at the end of the string
   role_name                   = format(var.read_write_terraform_state_role_name, replace(var.assessment_account_name, "/ \\((?P<env_type>[[:alnum:]]*)\\)$/", "-$env_type"))
