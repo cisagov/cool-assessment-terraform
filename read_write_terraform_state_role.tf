01488cee09eaabe85b9e379034a1a5488a4d854c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-10-16 12:55:11 -0400

    Revert to using the default branch of cisagov/terraform-state-read-role-tf-module
    We can do this now that cisagov/terraform-state-read-role-tf-module#40
    has been merged.

--- read_write_terraform_state_role.tf
+++ read_write_terraform_state_role.tf
@@ -5,7 +5,7 @@
 # ------------------------------------------------------------------------------
 
 module "read_write_terraform_state" {
-  source = "github.com/cisagov/terraform-state-read-role-tf-module?ref=improvement%2Fadd-extra-read-only-states"
+  source = "github.com/cisagov/terraform-state-read-role-tf-module"
 
   providers = {
     aws = aws.provisionterraform
