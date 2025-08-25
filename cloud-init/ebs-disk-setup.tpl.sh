14232ab29a7532dd94826aa12013198a970ae51e
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-08-25 10:53:37 -0400

    Remove code that identifies the NVMe device name
    This code no longer works because nvme id-ctrl -v no longer shows the
    non-NVMe device name.  The non-NVMe device name shows up in Linux as a
    symlink to the appropriate NVMe device, though, so none of this
    chicanery is actually necessary.  We can use the non-NVMe device name
    directly.

--- cloud-init/ebs-disk-setup.tpl.sh
+++ cloud-init/ebs-disk-setup.tpl.sh
@@ -7,8 +7,8 @@
 # Input variables are:
 # num_disks - the number of extra (non-root) disks that are expected
 # to be attached
-# device_name - the old-style device name for the disk of interest
-# (/dev/xvdb, for example)
+# device_name - the device name for the disk of interest (/dev/xvdb,
+# for example)
 # mount_point - the directory where the disk is to be mounted
 # label - the label to give the disk if it is necessary to create a
 # file system
@@ -30,44 +30,19 @@ while [ "$(lsblk | grep --count ' disk')" -lt "${num_disks}" ]; do
   sleep 5
 done
 
-nvme_devices="$(find /dev | grep --ignore-case 'nvme[0-9][1-9]\?n1$')"
-# Find our device from among the NVMe devices by checking each one's
-# vendor-specific region for the non-NVMe device name as it is
-# specified in the Terraform code.  (AWS is nice enough to stash it
-# there for us.)
-#
-# This is important because with multiple NVMe devices the order is
-# non-deterministic.
+# Create a file system on the EBS volume if one was not already there.
+blkid --cache-file /dev/null "${device_name}" || mkfs --type "${fs_type}" -L "${label}" "${device_name}"
 
-for nvme_device in $nvme_devices; do
-  # Turn off pipefail and errexit for this one command.  This
-  # command will always fail if the NVMe disk isn't the one we're
-  # looking for.
-  set +o errexit
-  set +o pipefail
-  non_nvme_device_name=$(nvme id-ctrl -v "$nvme_device" | grep -o "${device_name}")
-  set -o errexit
-  set -o pipefail
+# Grab the UUID of this volume
+uuid=$(blkid --match-tag UUID --output value "${device_name}")
 
-  if [ "$non_nvme_device_name" = "${device_name}" ]; then
-    # We've found our device
+# Make the mount point directory (if it doesn't already exist)
+mkdir --parents "${mount_point}"
 
-    # Create a file system on the EBS volume if one was not
-    # already there.
-    blkid --cache-file /dev/null "$nvme_device" || mkfs --type "${fs_type}" -L "${label}" "$nvme_device"
+# Mount the file system
+mount UUID="$uuid" --options "${mount_options}" "${mount_point}"
 
-    # Grab the UUID of this volume
-    uuid=$(blkid --match-tag UUID --output value "$nvme_device")
-
-    # Make the mount point directory (if it doesn't already exist)
-    mkdir --parents "${mount_point}"
-
-    # Mount the file system
-    mount UUID="$uuid" --options "${mount_options}" "${mount_point}"
-
-    # Save the mount point in fstab, so the file system is
-    # remounted if the instance is rebooted
-    echo "# ${label}" >> /etc/fstab
-    echo "UUID=$uuid ${mount_point} ${fs_type} ${mount_options} 0 2" >> /etc/fstab
-  fi
-done
+# Save the mount point in fstab, so the file system is remounted if
+# the instance is rebooted
+echo "# ${label}" >> /etc/fstab
+echo "UUID=$uuid ${mount_point} ${fs_type} ${mount_options} 0 2" >> /etc/fstab
