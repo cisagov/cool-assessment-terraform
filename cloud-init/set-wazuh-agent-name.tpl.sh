b2969687b8aaebcbf15a98487586b4d9e7ba4124
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-03-02 11:03:58 -0500

    Add code to ensure idempotence
    - Only create the enrollment node if it does not already exist.
    - Only create the agent_name node if it does not already exist;
    otherwise, simply ensure it has the correct value.

--- cloud-init/set-wazuh-agent-name.tpl.sh
+++ cloud-init/set-wazuh-agent-name.tpl.sh
@@ -30,17 +30,39 @@ TEMP_FILE=$(mktemp)
   echo "</root>"
 } > "$TEMP_FILE"
 
-# Add the enrollment node
-xmlstarlet edit --omit-decl --inplace \
-  --subnode "/root/ossec_config/client" \
-  --type elem -n enrollment \
-  "$TEMP_FILE"
-# Now add the agent_name node
-xmlstarlet edit --omit-decl --inplace \
-  --subnode "/root/ossec_config/client/enrollment" \
-  --type elem -n agent_name \
-  --value "${deployment_name}.${account_name}.${hostname}" \
-  "$TEMP_FILE"
+# Check if the enrollment node exists and add it if it does not.
+COUNT=$(xmlstarlet select \
+  --template \
+  --value-of 'count(/root/ossec_config/client/enrollment)' \
+  "$TEMP_FILE")
+if [ "$COUNT" -le 0 ]; then
+  # Add the enrollment node
+  xmlstarlet edit --omit-decl --inplace \
+    --subnode "/root/ossec_config/client" \
+    --type elem -n enrollment \
+    "$TEMP_FILE"
+fi
+
+# Check if the agent_name node exists and create it if it does not.
+# If it already exists, ensure that it has the correct value.
+COUNT=$(xmlstarlet select \
+  --template \
+  --value-of 'count(/root/ossec_config/client/enrollment/agent_name)' \
+  "$TEMP_FILE")
+if [ "$COUNT" -gt 0 ]; then
+  # agent_name exists, so simply ensure it has the correct value
+  xmlstarlet edit --omit-decl --inplace \
+    --update "/root/ossec_config/client/enrollment/agent_name" \
+    --value "${deployment_name}.${account_name}.${hostname}" \
+    "$TEMP_FILE"
+else
+  # agent_name does not exist, so create it
+  xmlstarlet edit --omit-decl --inplace \
+    --subnode "/root/ossec_config/client/enrollment" \
+    --type elem -n agent_name \
+    --value "${deployment_name}.${account_name}.${hostname}" \
+    "$TEMP_FILE"
+fi
 
 # Now remove the fake root tag by deleting the first and last lines of
 # the file.
