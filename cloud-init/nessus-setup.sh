5d989d43ef9e856b44c96176fbedacf47c59c2e7
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-07-07 16:38:22 -0400

    Add code to limit the number of attempts to set the Nessus web port
    Co-authored-by: David Redmin <david.redmin@gwe.cisa.dhs.gov>

--- cloud-init/nessus-setup.sh
+++ cloud-init/nessus-setup.sh
@@ -77,17 +77,26 @@ else
 fi
 
 # Set web server port
+max_attempts=10
+attempt_count=0
 seconds_to_sleep=60
-while true; do
+while [ $((++attempt_count)) -le $max_attempts ]; do
   # shellcheck disable=SC2154
-  echo "Attempting to set Nessus web server port to ${nessus_web_server_port}..."
+  echo "Attempt $attempt_count of $max_attempts to set Nessus web server port to ${nessus_web_server_port}..."
   $nessus_sbin_path/nessuscli fix --set xmlrpc_listen_port="${nessus_web_server_port}"
   xmlrpc_listen_port_rc="$?"
 
   if [ "$xmlrpc_listen_port_rc" -ne 0 ]; then
-    echo -n "ERROR: Setting Nessus web server port was unsuccessful.  "
-    echo Will retry in $seconds_to_sleep seconds.
-    sleep $seconds_to_sleep
+    if [ $attempt_count -lt $max_attempts ]; then
+      # We will try again
+      echo -n "ERROR: Setting Nessus web server port was unsuccessful.  "
+      echo Will retry in $seconds_to_sleep seconds.
+      sleep $seconds_to_sleep
+    else
+      # No more retries
+      echo "Error: Unable to set Nessus web server port to ${nessus_web_server_port} after $attempt_count attempts"
+      exit $xmlrpc_listen_port_rc
+    fi
   else
     # No need to echo anything if successful since nessuscli will do
     # it for us
